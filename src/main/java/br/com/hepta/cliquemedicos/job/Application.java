d23220fd370cbd84efe4a4e3ac6358cce39c805b
Bruno Carneiro <bruno.carneiro312@gmail.com>
2019-08-29 17:37:48 -0300

    Configurando Log4J

--- src/main/java/br/com/hepta/cliquemedicos/job/Application.java
+++ src/main/java/br/com/hepta/cliquemedicos/job/Application.java
@@ -1,9 +1,18 @@
 package br.com.hepta.cliquemedicos.job;
 
+import org.apache.log4j.Logger;
+
 public class Application {
+
+    final static Logger logger = Logger.getLogger(Application.class);
 
     public static void main(String[] args) {
         System.out.println("Application");
+        logger.debug("Application");
+        logger.info("Application");
+        logger.warn("Application");
+        logger.error("Application");
+        logger.fatal("Application");
     }
 
 }
